524a78a69bcae7cad08bcbacdc5a478748bbc8f6
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-03-24 08:14:54 +0100

    refactor: automatically register all models within their respective apps to the Django admin site.

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app = apps.get_app_config('user')
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
