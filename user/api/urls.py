c063efcc10202fe93e3d6e2446f93e21562f74f9
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-03-24 13:22:05 +0100

    feat: Import new views for user authentication, registration, and management.

--- user/api/urls.py
+++ user/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from user.api.views.auth import (
+    CustomTokenObtainPairView,
+    MeView,
+    RefreshView,
+    RegisterView,
+    ResendVerificationEmailView,
     UserSerializer,
     VerifyEmailConfirmView,
     UserConfigView,
